c84e644a80222aab01f1e69dab3086cbbccb02f8
Mahesh <maheshkarande1985@gmail.com>
2023-02-02 13:36:27 +0530

    BottomSheetAdded

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,14 +1,19 @@
 import 'package:date_picker_timeline/date_picker_timeline.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
+import 'package:todoapp/controllers/task_controller.dart';
+import 'package:todoapp/models/task.dart';
 import 'package:todoapp/services/notification_service.dart';
 import 'package:todoapp/services/theme_services.dart';
 import 'package:todoapp/ui/add_task_bar.dart';
 import 'package:todoapp/ui/theme.dart';
 import 'package:todoapp/ui/widget/button.dart';
+import 'package:todoapp/ui/add_task_bar.dart';
+import 'package:todoapp/ui/widget/task_tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -19,6 +24,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   DateTime _selectedDate = DateTime.now();
+  final _taskController = Get.put(TaskController());
   var notifyHelper;
   @override
   void initState() {
@@ -38,9 +44,41 @@ class _HomePageState extends State<HomePage> {
         children: [
           _addTaskBar(),
           _addDateBar(),
+          SizedBox(height: 10),
+          _showTasks()
         ],
       ),
     );
+  }
+
+  _showTasks() {
+    return Expanded(child: Obx(
+      () {
+        return ListView.builder(
+          itemCount: _taskController.taskList.length,
+          itemBuilder: (_, index) {
+            print(_taskController.taskList.length);
+            return AnimationConfiguration.staggeredList(
+              position: index,
+              child: SlideAnimation(
+                child: FadeInAnimation(
+                  child: Row(
+                    children: [
+                      GestureDetector(
+                          onTap: () {
+                            _showBottomSheet(
+                                context, _taskController.taskList[index]);
+                          },
+                          child: TaskTile(_taskController.taskList[index]))
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        );
+      },
+    ));
   }
 
   _appBar() {
@@ -74,38 +112,98 @@ class _HomePageState extends State<HomePage> {
       ],
     );
   }
+
+  _addTaskBar() {
+    return Container(
+      margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  DateFormat.yMMMMd().format(
+                    DateTime.now(),
+                  ),
+                  style: subHeadingStyle,
+                ),
+                Text(
+                  "Today",
+                  style: headingStyle,
+                ),
+              ],
+            ),
+          ),
+          MyButton(
+              label: "+ Add Task",
+              onTap: () async {
+                await Get.to(() => AddTaskPage());
+                _taskController.getTasks();
+              }),
+        ],
+      ),
+    );
+  }
 }
 
-_addTaskBar() {
-  return Container(
-    margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                DateFormat.yMMMMd().format(
-                  DateTime.now(),
-                ),
-                style: subHeadingStyle,
-              ),
-              Text(
-                "Today",
-                style: headingStyle,
-              ),
-            ],
+_showBottomSheet(BuildContext context, Task task) {
+  Get.bottomSheet(
+    Container(
+      padding: const EdgeInsets.only(top: 4),
+      height: task.isCompleted == 1
+          ? MediaQuery.of(context).size.height * 0.24
+          : MediaQuery.of(context).size.height * 0.32,
+      color: Get.isDarkMode ? darkGreyClr : Colors.white,
+      child: Column(
+        children: [
+          Container(
+            height: 6,
+            width: 120,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Get.isDarkMode ? Colors.grey[600] : Colors.grey[300]),
           ),
+          task.isCompleted == 1
+              ? Container()
+              : _bottomSheetButton(
+                  label: "Task Completed",
+                  onTap: () {
+                    Get.back();
+                  },
+                  clr: primaryClr,
+                  context:context
+                  )
+        ],
+      ),
+    ),
+  );
+}
+
+_bottomSheetButton(
+    {required String label,
+    required Function()? onTap,
+    required Color clr,
+    bool isClose = false,
+    required BuildContext context
+    }) {
+  return GestureDetector(
+    onTap: onTap,
+    child: Container(
+      margin: const EdgeInsets.symmetric(vertical: 4),
+      height: 55,
+      width:MediaQuery.of(context).size.width*0.9 ,
+      
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 2,
+          color: isClose == true?Colors.red:clr
         ),
-        MyButton(
-          label: "+ Add Task",
-          onTap: () => Get.to(
-            AddTaskPage(),
-          ),
-        ),
-      ],
+        borderRadius:BorderRadius.circular(20),
+        color: isClose == true?Colors.red:clr
+      ),
+       
     ),
   );
 }
